380c9fa0a8fb657c28174620dd41527cd304e001
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-02-05 16:53:03 +0100

    Blink1 commands are now assembled from proper settings file (dust.js)

--- app.js
+++ app.js
@@ -1,32 +1,93 @@
+/**
+ * Copyright (C) 2013, Oliver Salzburg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Created: 2013-02-04 16:19
+ *
+ * @author Oliver Salzburg
+ * @copyright Copyright (C) 2012, Oliver Salzburg
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+
 var dgram = require( "dgram" );
 var udpServer = dgram.createSocket( "udp4" );
 var Blink1 = require( "node-blink1" );
 var blink1 = new Blink1.Blink1();
 
+var options = require( __dirname + "/dust.js" );
+
+/**
+ * Helper function to wrap a function (Yes, you read that right)
+ * @param fn
+ * @param context
+ * @param params
+ * @returns {Function}
+ * @source http://stackoverflow.com/questions/899102/how-do-i-store-javascript-functions-in-a-queue-for-them-to-be-executed-eventuall
+ */
+var wrapFunction = function(fn, context, params) {
+  return function() {
+    fn.apply(context, params);
+  };
+};
+
+/**
+ * Invoked when the UDP listener becomes ready.
+ */
 udpServer.on( "listening", function() {
   var address = udpServer.address();
   console.log( "State change listener ready on " + address.address + ":" + address.port );
 } );
 
-// Call this when we receive a UDP message
+/**
+ * Invoked when a UDP message is received
+ */
 udpServer.on( "message", function( msg, rinfo ) {
   var buffer = new Buffer( msg );
 
   var state = buffer.toString();
   console.log( "Got state " + state + " from " + rinfo.address + ":" + rinfo.port );
-  blink( "A" == state );
+
+  // Get the pixie dust for this message
+  var pixieDust = getPixieDustForMessage( state );
+  blink( pixieDust );
 } );
 
+/**
+ * Retrieve the pixie dust recipe for a given UDP message
+ * @param message
+ * @returns {*}
+ */
+function getPixieDustForMessage( message ) {
+  return options[ message ];
+}
 
-function blink( state ) {
-  if( state ) {
-    blink1.fadeToRGB( 500, 0, 255, 0, function() {
-      blink1.fadeToRGB( 500, 0, 0, 0 );
-    } );
-  } else {
-    blink1.fadeToRGB( 500, 255, 0, 0, function() {
-      blink1.fadeToRGB( 500, 0, 0, 0 );
-    } );
+/**
+ * Blink a pixie dust recipe
+ * @param pixieDust The recipe to blink
+ */
+function blink( pixieDust ) {
+  var parameters = pixieDust.shift();
+  if( parameters ) {
+    var callback = wrapFunction( blink, this, [ pixieDust ] );
+    parameters.push( callback );
+    blink1.fadeToRGB.apply( blink1, parameters );
   }
 }
 
---
+++ dust.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2013, Oliver Salzburg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Created: 2013-02-05 15:56
+ *
+ * @author Oliver Salzburg
+ * @copyright Copyright (C) 2012, Oliver Salzburg
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+var dust = {
+  "A": [
+    [500, 0, 255, 0],
+    [500, 0, 0, 0]
+  ],
+  "U": [
+    [500, 255, 0, 0],
+    [500, 0, 0, 0]
+  ]
+};
+
+module.exports = dust;
